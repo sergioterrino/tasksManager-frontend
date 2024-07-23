1c2c324ca679177695fc25158b1b80172ad8371f
Sergio Terrino <120554805+sergioterrino@users.noreply.github.com>
2024-07-23 13:54:57 +0200

    6 commit - update de meta.env.VITE_API_URL para que se setee en funcion del .env(prod/dev)

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 const apiUrl = import.meta.env.VITE_API_URL;
-const apiUrlRender = import.meta.env.VITE_API_URL_RENDER; // url desplegada
+// const apiUrlRender = import.meta.env.VITE_API_URL_RENDER; // url desplegada
 
 const instance = axios.create({
-  baseURL: apiUrlRender,
+  baseURL: apiUrl,
   withCredentials: true // xra que setee las cookies
 })
 
