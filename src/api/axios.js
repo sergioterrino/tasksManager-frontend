9bf7c287e208aa99f932bada1ab2dfe4faa5bd2b
Sergio Terrino <120554805+sergioterrino@users.noreply.github.com>
2024-07-24 11:55:30 +0200

    10 commit - restauro la apiURl en axios.js para que sea en funcion del .env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 const apiUrl = import.meta.env.VITE_API_URL;
-// const apiUrlRender = import.meta.env.VITE_API_URL_RENDER; // url desplegada
 
 const instance = axios.create({
-  baseURL: "https://tasksmanager-backend.onrender.com/api", // url desplegada
+  baseURL: apiUrl,
   withCredentials: true // xra que setee las cookies
 })
 
