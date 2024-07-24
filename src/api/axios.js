81133beaadbaf4195b216008326215db1fd8b110
Sergio Terrino <120554805+sergioterrino@users.noreply.github.com>
2024-07-24 02:05:45 +0200

    9 commit- pongo manual la urta del backend en el client

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ const apiUrl = import.meta.env.VITE_API_URL;
 // const apiUrlRender = import.meta.env.VITE_API_URL_RENDER; // url desplegada
 
 const instance = axios.create({
-  baseURL: apiUrl,
+  baseURL: "https://tasksmanager-backend.onrender.com/api", // url desplegada
   withCredentials: true // xra que setee las cookies
 })
 
